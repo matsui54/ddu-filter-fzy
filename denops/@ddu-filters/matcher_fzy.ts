8e7b0480eefd17b3ff5b8cbd02361437368c217b
matsui54 <haru.matu9168@gmail.com>
2022-03-01 14:04:08 +0900

    Remove previous highlights

--- denops/@ddu-filters/matcher_fzy.ts
+++ denops/@ddu-filters/matcher_fzy.ts
@@ -38,6 +38,7 @@ export class Filter extends BaseFilter<Params> {
         c,
       ) => ({ score: score(input, c.word), item: c } as SortItem))
         .sort((a, b) => b.score - a.score).map((c) => {
+          const prev = c.item.highlights?.filter((h) => h.name != "ddu_fzy_hl");
           const highlights = positions(input, c.item.word).map((p) => ({
             col: p + 1,
             type: "abbr",
@@ -45,8 +46,8 @@ export class Filter extends BaseFilter<Params> {
             "hl_group": args.filterParams.hlGroup,
             width: 1,
           }));
-          if (c.item.highlights) {
-            c.item.highlights = c.item.highlights.concat(highlights);
+          if (prev) {
+            c.item.highlights = prev.concat(highlights);
           } else {
             c.item.highlights = highlights;
           }
